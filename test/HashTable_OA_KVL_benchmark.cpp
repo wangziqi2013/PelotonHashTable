7386cf83e9ae0d6f3cf3d00d349294332e25fbd9
= <675973885@qq.com>
2016-09-19 20:42:07 -0400

    Adding comparison test cases using std::unordered_map

--- test/HashTable_OA_KVL_benchmark.cpp
+++ test/HashTable_OA_KVL_benchmark.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <random>
 #include <chrono>
+#include "unordered_map"
 
 
 using namespace peloton;
@@ -13,7 +14,11 @@ void SequentialInsertTest(uint64_t key_num) {
   start = std::chrono::system_clock::now();
 
   // Insert 1 million keys into std::map
-  HashTable_OA_KVL<uint64_t, uint64_t, SimpleInt64Hasher> test_map{1024};
+  HashTable_OA_KVL<uint64_t,
+                   uint64_t,
+                   SimpleInt64Hasher,
+                   std::equal_to<uint64_t>,
+                   LoadFactorThreeFourthFull> test_map{1024};
   for(uint64_t i = 0;i < key_num;i++) {
     test_map.Insert(i, i);
   }
@@ -52,8 +57,53 @@ void SequentialInsertTest(uint64_t key_num) {
   return;
 }
 
+void UnorderedMapSequentialInsertTest(uint64_t key_num) {
+  std::chrono::time_point<std::chrono::system_clock> start, end;
+  start = std::chrono::system_clock::now();
+
+  // Insert 1 million keys into std::map
+  std::unordered_map<uint64_t, uint64_t> test_map{};
+  for(uint64_t i = 0;i < key_num;i++) {
+    test_map[i] = i;
+  }
+
+  end = std::chrono::system_clock::now();
+
+  std::chrono::duration<double> elapsed_seconds = end - start;
+
+  std::cout << "std::unordered_map: " << 1.0 * key_num / (1024 * 1024) / elapsed_seconds.count()
+            << " million insertion/sec" << "\n";
+
+  ////////////////////////////////////////////
+  // Test read
+  std::vector<uint64_t> v{};
+  v.reserve(100);
+
+  start = std::chrono::system_clock::now();
+
+  int iter = 10;
+  for(int j = 0;j < iter;j++) {
+    // Read 1 million keys from std::map
+    for(uint64_t i = 0;i < key_num;i++) {
+      uint64_t t = test_map[i];
+
+      v.push_back(t);
+      v.clear();
+    }
+  }
+
+  end = std::chrono::system_clock::now();
+
+  elapsed_seconds = end - start;
+  std::cout << "std::unordered_map: " << (1.0 * iter * key_num) / (1024 * 1024) / elapsed_seconds.count()
+            << " million read/sec" << "\n";
+
+  return;
+}
+
 int main() {
   SequentialInsertTest(6 * 1024 * 1024);
+  UnorderedMapSequentialInsertTest(6 * 1024 * 1024);
   
   return 0;
 }
